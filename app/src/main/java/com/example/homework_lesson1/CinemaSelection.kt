5c8e6549ad9218585d799600c341a4c555916c13
valeriykov <valeriy.kov@gmail.com>
2022-07-25 23:09:41 +0500

    CinemaSelection add createCinemaList

---
+++ app/src/main/java/com/example/homework_lesson1/CinemaSelection.kt
@@ -0,0 +1,61 @@
+package com.example.homework_lesson1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.homework_lesson1.databinding.ActivityCinemaSelectionBinding
+import com.example.homework_lesson1.databinding.ItemCinemaBinding
+import com.example.homework_lesson1.model.Options
+import kotlin.properties.Delegates
+
+class CinemaSelection : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCinemaSelectionBinding
+    private var cinemaIndex by Delegates.notNull<Int>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCinemaSelectionBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        cinemaIndex = savedInstanceState?.getInt(KEY_CINEMA_INDEX) ?: -1
+        val options = intent.getParcelableExtra<Options>(EXTRA_OPTIONS) ?: throw IllegalArgumentException("Can't start without options")
+
+        createCinemaList(binding, options.count)
+    }
+
+    private fun createCinemaList(binding: ActivityCinemaSelectionBinding, count: Int){
+        val mapIterator = image_resurces.iterator()
+        val cinemaBindings = (0 until count).map { index ->
+            val cinemaBinding = ItemCinemaBinding.inflate(layoutInflater)
+            cinemaBinding.root.id = View.generateViewId()
+            val next = mapIterator.next()
+            cinemaBinding.cinemaImageView.setImageResource(next.second)
+            cinemaBinding.cinemaTitleTextView.text = next.first
+            cinemaBinding.root.tag = index
+            cinemaBinding.root.setOnClickListener { onItemSelected(it) }
+            binding.root.addView(cinemaBinding.root)
+            cinemaBinding
+        }
+        binding.flowCinema.referencedIds = cinemaBindings.map { it.root.id }.toIntArray()
+    }
+
+    private fun onItemSelected(view: View){
+        if (view.tag as? Int == cinemaIndex) {
+//            val intent = Intent(this, ViewCinema:: class.java)
+//            startActivity(intent)
+        }
+    }
+
+    companion object {
+        const val KEY_CINEMA_INDEX = "key_cinema_index"
+        const val EXTRA_OPTIONS = "extra_options"
+
+        val image_resurces = sequenceOf(
+            "Savage" to R.drawable.savage,
+            "Avatar 2" to R.drawable.avatar_2,
+            "Force Works" to R.drawable.force_works,
+            "Eternal Warrior" to R.drawable.eternal_warrior,
+            "Siege" to R.drawable.siege
+        )
+    }
+}
---
+++ app/src/main/java/com/example/homework_lesson1/model/Options.kt
@@ -0,0 +1,28 @@
+package com.example.homework_lesson1.model
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Options(val count: Int): Parcelable {
+
+    constructor(parcel: Parcel) : this(parcel.readInt()) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(count)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Options> {
+        override fun createFromParcel(parcel: Parcel): Options {
+            return Options(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Options?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
