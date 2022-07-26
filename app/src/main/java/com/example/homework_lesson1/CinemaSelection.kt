06eed15211480453b5eb11817d662bc88d526b86
valeriykov <valeriy.kov@gmail.com>
2022-07-27 00:12:08 +0500

    save image id and description

--- app/src/main/java/com/example/homework_lesson1/CinemaSelection.kt
+++ app/src/main/java/com/example/homework_lesson1/CinemaSelection.kt
@@ -4,14 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.homework_lesson1.databinding.ActivityCinemaSelectionBinding
+import com.example.homework_lesson1.databinding.ActivityMainBinding
 import com.example.homework_lesson1.databinding.ItemCinemaBinding
 import com.example.homework_lesson1.model.Options
 import kotlin.properties.Delegates
 
 class CinemaSelection : AppCompatActivity() {
 
+    private lateinit var binding: ActivityCinemaSelectionBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cinema_selection)
+        binding = ActivityCinemaSelectionBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        val desc = intent.getStringExtra(MainActivity.EXTRA_CINEMA_INFO) ?: throw IllegalArgumentException("Can't start without cinema description")
+        val id = intent.getIntExtra(MainActivity.EXTRA_IMAGE_ID, -1)
+        id.takeIf { it > 0 }?.let { binding.cinemaImageView.setImageResource(it) }
+        binding.cinemaInfoTextView.text = desc
     }
 }
--- app/src/main/java/com/example/homework_lesson1/MainActivity.kt
+++ app/src/main/java/com/example/homework_lesson1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
 import com.example.homework_lesson1.databinding.ActivityMainBinding
 import com.example.homework_lesson1.databinding.ItemCinemaBinding
 import com.example.homework_lesson1.model.Options
@@ -34,7 +35,9 @@ class MainActivity : AppCompatActivity() {
     override fun onRestart() {
         super.onRestart()
         if (cinemaIndex > 0) {
-            binding
+            val cinemaBinding = binding.root.getViewById(cinemaIndex).tag as? ItemCinemaBinding
+            cinemaBinding?.cinemaTitleTextView?.setTextColor(R.color.red.toInt())
+            this.onContentChanged()
         }
     }
 
@@ -46,25 +49,40 @@ class MainActivity : AppCompatActivity() {
             val next = mapIterator.next()
             cinemaBinding.cinemaImageView.setImageResource(next.second)
             cinemaBinding.cinemaTitleTextView.text = next.first
-            cinemaBinding.root.tag = next.second
-            cinemaBinding.cinemaDetailsButton.setOnClickListener { onItemSelected(cinemaBinding.root) }
+            cinemaBinding.root.tag = cinemaBinding
+            cinemaBinding.cinemaDetailsButton.setOnClickListener {
+                cinemaIndex = cinemaBinding.root.id
+                onItemSelected(next)
+            }
             binding.root.addView(cinemaBinding.root)
             cinemaBinding
         }
         binding.flowCinema.referencedIds = cinemaBindings.map { it.root.id }.toIntArray()
     }
 
-    private fun onItemSelected(view: View){
-        cinemaIndex = view.tag as? Int ?: -1
+    private fun onItemSelected(pair: Pair<String, Int>){
         if (cinemaIndex > 0) {
             val intent = Intent(this, CinemaSelection:: class.java)
+            intent.putExtra(EXTRA_IMAGE_ID, pair.second)
+            val description = mapCinemaInfo[pair.first]
+            description?.let { intent.putExtra(EXTRA_CINEMA_INFO, description) }
             startActivity(intent)
         }
     }
 
     companion object {
         const val KEY_CINEMA_INDEX = "key_cinema_index"
-        const val KEY_OPTIONS = "extra_options"
+        const val KEY_OPTIONS = "key_options"
+        const val EXTRA_IMAGE_ID = "extra_image_id"
+        const val EXTRA_CINEMA_INFO = "extra_cinema_info"
+
+        val mapCinemaInfo = mapOf(
+            "Savage" to "Уэсли Гибсон - зануда и нытик, целыми днями торчащий в офисе. Босс его унижает, подружка ему изменяет. Его жизнь катится в никуда! Обнаружив, что отец, бросивший его в раннем детстве, зверски убит, Уэс оказывается втянут в тайное общество киллеров под названием «Братство». За его обучение берется опытная наставница Фокс, и этот когда-то прикованный к офисному креслу хлюпик развивает молниеносную реакцию и сверхчеловеческую скорость. Однако уже на первом задании Уэсли понимает: есть вещи посложнее, чем вершить судьбы других людей. Это - набраться храбрости и стать хозяином собственной жизни. ",
+            "Avatar 2" to "После принятия образа аватара солдат Джейк Салли становится предводителем народа нави и берет на себя миссию по защите новых друзей от корыстных бизнесменов с Земли. Теперь ему есть за кого бороться — с Джейком его прекрасная возлюбленная Нейтири. Когда на Пандору возвращаются до зубов вооруженные земляне, Джейк готов дать им отпор.",
+            "Force Works" to "1942 год, оккупированная Европа. Оказавшись в концлагере, бельгиец еврейского происхождения Жиль Кремье выдает себя за перса — для него это единственная возможность остаться в живых. Эта ложь действительно спасает ему жизнь, но Жиль еще не представляет, какой ценой. Довольные таким редким уловом немецкие солдаты приводят Жиля к концлагерному повару Клаусу Коху, который мечтает, как только закончится война, уехать в Иран и открыть там ресторан. Кох ищет настоящего перса, который научит его говорить на персидском языке. Так начинается история Жиля Кремье и Клауса Коха — еврея и немца, узника и тюремщика, учителя и ученика.",
+            "Eternal Warrior" to "Франция, конец XIV века. Отношения между рыцарем Жаном де Карружем и его соратником и соседом Жаком Ле Гри постепенно портятся, и дело доходит до неразрешимого конфликта. Оскорблённый Карруж обращается за помощью к королю Франции Карлу VI, и тот постановляет — противники должны решить спор в смертельной дуэли.",
+            "Siege" to "Когда в Бруклине взлетает на воздух автобус и на улицах Нью-Йорка разворачивается кровавый террор, Хаб — Хаббарт, глава Объединенного Антитеррористического Подразделения ФБР получает задание задержать террористов и предъявить им обвинение. Элиза Крафт — тайный оперативный агент ЦРУ. Она обладает ценными источниками информации в среде американцев арабского происхождения и тесно связана с ними. Хаб и Элиза работают вместе, объединившись для совместной борьбы против террористической группы. Но этого оказывается недостаточно. Требования общественности обеспечить безопасность граждан вынуждают президента США объявить чрезвычайное положение и обратиться за помощью к армии..."
+        )
 
         val image_resurces = sequenceOf(
             "Savage" to R.drawable.savage,
